aed57687c9025944a84cb432bc99cee2d49225cd
Matteus Isaque <matteus.isaque28@gmail.com>
2023-12-22 09:21:26 -0300

    alt

--- app.ts
+++ app.ts
@@ -9,13 +9,11 @@ import bodyParser from "body-parser";
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(cors());
 DotEnv.config();
 
-
 app.use(router);
-
 
 // app.get("/mercado_pago/planos", async (req: Request, res: Response) => {
 //   const b: any = await MercadoPagoApi.get("preapproval_plan/search");
@@ -25,6 +23,6 @@ app.use(router);
 //   res.status(201).json({ c });
 // });
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || "3333", () => {
   console.log("rodando");
 });
